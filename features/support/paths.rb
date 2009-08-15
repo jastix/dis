d8ecbb385085982d109a5484821571281c235b5e
jastix <nik@jastix.me>
2009-08-15 13:33:17 +0400

    Clearance features pass

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -13,7 +13,13 @@ module NavigationHelpers
 
     when /list of themes/
       themes_path
-
+    # clearance routes
+    when /the sign up page/i
+      new_user_path
+    when /the sign in page/i
+      new_session_path
+    when /the password reset request page/i
+      new_password_path
 
     # Add more mappings here.
     # Here is a more fancy example:
